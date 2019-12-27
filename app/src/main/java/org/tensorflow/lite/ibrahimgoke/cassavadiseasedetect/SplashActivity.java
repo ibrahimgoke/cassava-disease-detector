9c3cd0fac53cb0c9aaff255e670e773299c5da7c
Ibrahim <olagokeibrahim165@gmail.com>
2019-12-27 16:28:19 +0100

    Splash Screen Added

---
+++ app/src/main/java/org/tensorflow/lite/ibrahimgoke/cassavadiseasedetect/SplashActivity.java
@@ -0,0 +1,27 @@
+package org.tensorflow.lite.ibrahimgoke.cassavadiseasedetect;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,ClassifierActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
